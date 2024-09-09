fa0a78a837e0333b585c74a4f9af94bc9f3a7953
nopnop2002 <hau26280@tree.odn.ne.jp>
2024-09-09 11:17:01 +0900

    added DeveiceAddDemo

---
+++ DeviceAddDemo/main/main.c
@@ -0,0 +1,268 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#if CONFIG_LEGACY_DRIVER
+#include "driver/i2c.h"
+#else
+#include "driver/i2c_master.h"
+#endif
+
+#include "ssd1306.h"
+#include "font8x8_basic.h"
+
+/*
+ You have to set this config value with menuconfig
+ CONFIG_INTERFACE
+
+ for i2c
+ CONFIG_MODEL
+ CONFIG_SDA_GPIO
+ CONFIG_SCL_GPIO
+ CONFIG_RESET_GPIO
+
+ for SPI
+ CONFIG_CS_GPIO
+ CONFIG_DC_GPIO
+ CONFIG_RESET_GPIO
+*/
+
+#define tag "SSD1306"
+
+void app_main(void)
+{
+	SSD1306_t dev;
+	int center, top, bottom;
+	char lineChar[20];
+
+#if CONFIG_I2C_INTERFACE
+	ESP_LOGI(tag, "INTERFACE is i2c");
+	ESP_LOGI(tag, "CONFIG_SDA_GPIO=%d",CONFIG_SDA_GPIO);
+	ESP_LOGI(tag, "CONFIG_SCL_GPIO=%d",CONFIG_SCL_GPIO);
+	ESP_LOGI(tag, "CONFIG_RESET_GPIO=%d",CONFIG_RESET_GPIO);
+
+#if CONFIG_I2C_PORT_0
+	i2c_port_t i2c_num = I2C_NUM_0;
+#else
+	i2c_port_t i2c_num = I2C_NUM_1;
+#endif
+
+#if CONFIG_LEGACY_DRIVER
+	ESP_LOGI(tag, "I2C_LEGACY_DRIVER");
+	// install i2c master driver
+    i2c_config_t i2c_config = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = CONFIG_SDA_GPIO,
+        .scl_io_num = CONFIG_SCL_GPIO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = 400000
+    };
+    ESP_ERROR_CHECK(i2c_param_config(i2c_num, &i2c_config));
+    ESP_ERROR_CHECK(i2c_driver_install(i2c_num, I2C_MODE_MASTER, 0, 0, 0));
+
+	// add new device to i2c bus
+	i2c_device_add(&dev, i2c_num, CONFIG_RESET_GPIO);
+#else
+	ESP_LOGI(tag, "I2C_NEW_DRIVER");
+	// install i2c master driver
+    i2c_master_bus_config_t i2c_mst_config = {
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .glitch_ignore_cnt = 7,
+        .i2c_port = i2c_num,
+        .scl_io_num = CONFIG_SCL_GPIO,
+        .sda_io_num = CONFIG_SDA_GPIO,
+        .flags.enable_internal_pullup = true,
+    };
+    i2c_master_bus_handle_t bus_handle;
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
+
+	// add new device to i2c bus
+	i2c_bus_add(&dev, bus_handle, i2c_num, CONFIG_RESET_GPIO);
+#endif
+
+#endif // CONFIG_I2C_INTERFACE
+
+#if CONFIG_SPI_INTERFACE
+	ESP_LOGI(tag, "INTERFACE is SPI");
+	ESP_LOGI(tag, "CONFIG_MOSI_GPIO=%d",CONFIG_MOSI_GPIO);
+	ESP_LOGI(tag, "CONFIG_SCLK_GPIO=%d",CONFIG_SCLK_GPIO);
+	ESP_LOGI(tag, "CONFIG_CS_GPIO=%d",CONFIG_CS_GPIO);
+	ESP_LOGI(tag, "CONFIG_DC_GPIO=%d",CONFIG_DC_GPIO);
+	ESP_LOGI(tag, "CONFIG_RESET_GPIO=%d",CONFIG_RESET_GPIO);
+
+    spi_bus_config_t spi_bus_config = {
+        .mosi_io_num = CONFIG_MOSI_GPIO,
+        .miso_io_num = -1,
+        .sclk_io_num = CONFIG_SCLK_GPIO,
+        .quadwp_io_num = -1,
+        .quadhd_io_num = -1,
+        .max_transfer_sz = 0,
+        .flags = 0
+    };
+
+#if CONFIG_SPI2_HOST
+	spi_host_device_t host_id = SPI2_HOST;
+#else
+	spi_host_device_t host_id = SPI3_HOST;
+#endif
+
+    ESP_LOGI(tag, "SPI HOST_ID=%d", host_id);
+    esp_err_t ret = spi_bus_initialize( host_id, &spi_bus_config, SPI_DMA_CH_AUTO );
+    ESP_LOGI(tag, "spi_bus_initialize=%d",ret);
+    assert(ret==ESP_OK);
+
+	spi_device_add(&dev, CONFIG_CS_GPIO, CONFIG_DC_GPIO, CONFIG_RESET_GPIO);
+#endif // CONFIG_SPI_INTERFACE
+
+#if CONFIG_FLIP
+	dev._flip = true;
+	ESP_LOGW(tag, "Flip upside down");
+#endif
+
+#if CONFIG_SSD1306_128x64
+	ESP_LOGI(tag, "Panel is 128x64");
+	ssd1306_init(&dev, 128, 64);
+#endif // CONFIG_SSD1306_128x64
+#if CONFIG_SSD1306_128x32
+	ESP_LOGI(tag, "Panel is 128x32");
+	ssd1306_init(&dev, 128, 32);
+#endif // CONFIG_SSD1306_128x32
+
+	ssd1306_clear_screen(&dev, false);
+	ssd1306_contrast(&dev, 0xff);
+	ssd1306_display_text_x3(&dev, 0, "Hello", 5, false);
+	vTaskDelay(3000 / portTICK_PERIOD_MS);
+
+#if CONFIG_SSD1306_128x64
+	top = 2;
+	center = 3;
+	bottom = 8;
+	ssd1306_display_text(&dev, 0, "SSD1306 128x64", 14, false);
+	ssd1306_display_text(&dev, 1, "ABCDEFGHIJKLMNOP", 16, false);
+	ssd1306_display_text(&dev, 2, "abcdefghijklmnop",16, false);
+	ssd1306_display_text(&dev, 3, "Hello World!!", 13, false);
+	//ssd1306_clear_line(&dev, 4, true);
+	//ssd1306_clear_line(&dev, 5, true);
+	//ssd1306_clear_line(&dev, 6, true);
+	//ssd1306_clear_line(&dev, 7, true);
+	ssd1306_display_text(&dev, 4, "SSD1306 128x64", 14, true);
+	ssd1306_display_text(&dev, 5, "ABCDEFGHIJKLMNOP", 16, true);
+	ssd1306_display_text(&dev, 6, "abcdefghijklmnop",16, true);
+	ssd1306_display_text(&dev, 7, "Hello World!!", 13, true);
+#endif // CONFIG_SSD1306_128x64
+
+#if CONFIG_SSD1306_128x32
+	top = 1;
+	center = 1;
+	bottom = 4;
+	ssd1306_display_text(&dev, 0, "SSD1306 128x32", 14, false);
+	ssd1306_display_text(&dev, 1, "Hello World!!", 13, false);
+	//ssd1306_clear_line(&dev, 2, true);
+	//ssd1306_clear_line(&dev, 3, true);
+	ssd1306_display_text(&dev, 2, "SSD1306 128x32", 14, true);
+	ssd1306_display_text(&dev, 3, "Hello World!!", 13, true);
+#endif // CONFIG_SSD1306_128x32
+	vTaskDelay(3000 / portTICK_PERIOD_MS);
+	
+	// Display Count Down
+	uint8_t image[24];
+	memset(image, 0, sizeof(image));
+	ssd1306_display_image(&dev, top, (6*8-1), image, sizeof(image));
+	ssd1306_display_image(&dev, top+1, (6*8-1), image, sizeof(image));
+	ssd1306_display_image(&dev, top+2, (6*8-1), image, sizeof(image));
+	for(int font=0x39;font>0x30;font--) {
+		memset(image, 0, sizeof(image));
+		ssd1306_display_image(&dev, top+1, (7*8-1), image, 8);
+		memcpy(image, font8x8_basic_tr[font], 8);
+		if (dev._flip) ssd1306_flip(image, 8);
+		ssd1306_display_image(&dev, top+1, (7*8-1), image, 8);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+	
+	// Scroll Up
+	ssd1306_clear_screen(&dev, false);
+	ssd1306_contrast(&dev, 0xff);
+	ssd1306_display_text(&dev, 0, "---Scroll  UP---", 16, true);
+	//ssd1306_software_scroll(&dev, 7, 1);
+	ssd1306_software_scroll(&dev, (dev._pages - 1), 1);
+	for (int line=0;line<bottom+10;line++) {
+		lineChar[0] = 0x01;
+		sprintf(&lineChar[1], " Line %02d", line);
+		ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
+		vTaskDelay(500 / portTICK_PERIOD_MS);
+	}
+	vTaskDelay(3000 / portTICK_PERIOD_MS);
+	
+	// Scroll Down
+	ssd1306_clear_screen(&dev, false);
+	ssd1306_contrast(&dev, 0xff);
+	ssd1306_display_text(&dev, 0, "--Scroll  DOWN--", 16, true);
+	//ssd1306_software_scroll(&dev, 1, 7);
+	ssd1306_software_scroll(&dev, 1, (dev._pages - 1) );
+	for (int line=0;line<bottom+10;line++) {
+		lineChar[0] = 0x02;
+		sprintf(&lineChar[1], " Line %02d", line);
+		ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
+		vTaskDelay(500 / portTICK_PERIOD_MS);
+	}
+	vTaskDelay(3000 / portTICK_PERIOD_MS);
+
+	// Page Down
+	ssd1306_clear_screen(&dev, false);
+	ssd1306_contrast(&dev, 0xff);
+	ssd1306_display_text(&dev, 0, "---Page	DOWN---", 16, true);
+	ssd1306_software_scroll(&dev, 1, (dev._pages-1) );
+	for (int line=0;line<bottom+10;line++) {
+		//if ( (line % 7) == 0) ssd1306_scroll_clear(&dev);
+		if ( (line % (dev._pages-1)) == 0) ssd1306_scroll_clear(&dev);
+		lineChar[0] = 0x02;
+		sprintf(&lineChar[1], " Line %02d", line);
+		ssd1306_scroll_text(&dev, lineChar, strlen(lineChar), false);
+		vTaskDelay(500 / portTICK_PERIOD_MS);
+	}
+	vTaskDelay(3000 / portTICK_PERIOD_MS);
+
+	// Horizontal Scroll
+	ssd1306_clear_screen(&dev, false);
+	ssd1306_contrast(&dev, 0xff);
+	ssd1306_display_text(&dev, center, "Horizontal", 10, false);
+	ssd1306_hardware_scroll(&dev, SCROLL_RIGHT);
+	vTaskDelay(5000 / portTICK_PERIOD_MS);
+	ssd1306_hardware_scroll(&dev, SCROLL_LEFT);
+	vTaskDelay(5000 / portTICK_PERIOD_MS);
+	ssd1306_hardware_scroll(&dev, SCROLL_STOP);
+	
+	// Vertical Scroll
+	ssd1306_clear_screen(&dev, false);
+	ssd1306_contrast(&dev, 0xff);
+	ssd1306_display_text(&dev, center, "Vertical", 8, false);
+	ssd1306_hardware_scroll(&dev, SCROLL_DOWN);
+	vTaskDelay(5000 / portTICK_PERIOD_MS);
+	ssd1306_hardware_scroll(&dev, SCROLL_UP);
+	vTaskDelay(5000 / portTICK_PERIOD_MS);
+	ssd1306_hardware_scroll(&dev, SCROLL_STOP);
+	
+	// Invert
+	ssd1306_clear_screen(&dev, true);
+	ssd1306_contrast(&dev, 0xff);
+	ssd1306_display_text(&dev, center, "  Good Bye!!", 12, true);
+	vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+
+	// Fade Out
+	ssd1306_fadeout(&dev);
+	
+#if 0
+	// Fade Out
+	for(int contrast=0xff;contrast>0;contrast=contrast-0x20) {
+		ssd1306_contrast(&dev, contrast);
+		vTaskDelay(40);
+	}
+#endif
+
+	// Restart module
+	esp_restart();
+}
